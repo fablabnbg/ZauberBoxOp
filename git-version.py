6c3fad10bc9bf57f8a0ffbe2942ab4d5f5ef1cdd
Ian Hubbertz <ian_web1@hubbertz.de>
2022-01-09 10:32:33 +0100

    Quick fix for git-version.py automatic version to use only version of last tag and ignore commit references

--- git-version.py
+++ git-version.py
@@ -6,7 +6,7 @@ def get_firmware_specifier_build_flag():
     #ret = subprocess.run(["git", "describe"], stdout=subprocess.PIPE, text=True) #Uses only annotated tags
     ret = subprocess.run(["git", "describe", "--tags"], stdout=subprocess.PIPE, text=True) #Uses any tags
     #ret = subprocess.run(["git", "describe", "--all"], stdout=subprocess.PIPE, text=True) #Uses any tags
-    build_version = ret.stdout.strip()
+    build_version = ret.stdout.strip().split('-')[0]
     build_flag = "-D AUTO_VERSION=\\\"" + build_version + "\\\""
     print ("Firmware Revision: " + build_version)
     return (build_flag)
